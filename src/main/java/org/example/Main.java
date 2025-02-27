1d69f151353b9adabbcda2cecc2d4322d4433be3
CristianCanoIng <canox09@gmail.com>
2025-02-27 12:34:41 -0500

    Piedra papel o tijera

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,69 +1,62 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner teclado = new Scanner(System.in);
-
-        List<String> productos = new ArrayList<>();
-        productos.add("1. Panela");
-        productos.add("2. Arroz");
-        productos.add("3. Chocolate");
-        productos.add("4. Papas");
-        productos.add("5. Cafe");
-
-        List<Double> precios = new ArrayList<>();
-        precios.add(2500.0);
-        precios.add(2000.0);
-        precios.add(1600.0);
-        precios.add(3000.0);
-        precios.add(5000.0);
-
-        int maxProductos = 5;
-
-        System.out.println("Bienvenido a la Maquinita\n");
-        System.out.println("Lista de productos:");
-
-        for (int i = 0; i < productos.size(); i++) {
-            System.out.println(productos.get(i) + " -> $" + precios.get(i));
-        }
-
-        System.out.print("Ingrese la cantidad de dinero que tiene: ");
-        double dineroDis = teclado.nextDouble();
-        double totalCompra = 0;
-
-        for (int i = 0; i < maxProductos; i++) {
-            System.out.print("Seleccione un producto (1-5) o 0 para salir: ");
-            int opcion = teclado.nextInt();
-
-            if (opcion == 0) {
-                break;
+        Random aleatorio = new Random();
+        int puntos1 = 0;
+        int puntos2 = 0;
+        int rondas = 0;
+        do {
+            int nRandom1 = aleatorio.nextInt(1, 3);
+            int nRandom2 = aleatorio.nextInt(1, 3);
+            if (nRandom1 == 1 && nRandom2 == 1 ) {
+                System.out.println("Jugador      1           2");
+                System.out.println("             Piedra      Piedra");
+                System.out.println("Empate\n");
+            } else if (nRandom1 == 1 && nRandom2 == 2) {
+                System.out.println("Jugador      1           2");
+                System.out.println("             Piedra      Papel");
+                System.out.println("Punto para el jugador 2\n");
+                puntos2 += 1;
+            } else if (nRandom1 == 1 && nRandom2 == 3) {
+                System.out.println("Jugador      1           2");
+                System.out.println("             Piedra      Tijera");
+                System.out.println("Punto para el jugador 1\n");
+                puntos1 += 1;
+            } else if (nRandom1 == 2 && nRandom2 == 1) {
+                System.out.println("Jugador      1           2");
+                System.out.println("             Papel       Piedra");
+                System.out.println("Punto para el jugador 1\n");
+                puntos1 += 1;
+            }else if (nRandom1 == 2 && nRandom2 == 2 ) {
+                System.out.println("Jugador      1           2");
+                System.out.println("             Papel       Papel");
+                System.out.println("Empate\n");
+            } else if (nRandom1 == 2 && nRandom2 == 3) {
+                System.out.println("Jugador      1           2");
+                System.out.println("             Papel       Tijera");
+                System.out.println("Punto para el jugador 2\n");
+                puntos2 += 1;
+            } else if (nRandom1 == 3 && nRandom2 == 1) {
+                System.out.println("Jugador      1           2");
+                System.out.println("             Tijera      Piedra");
+                System.out.println("Punto para el jugador 2\n");
+                puntos2 += 1;
+            } else if (nRandom1 == 3 && nRandom2 == 2) {
+                System.out.println("Jugador      1           2");
+                System.out.println("             Tijera       Papel");
+                System.out.println("Punto para el jugador 1\n");
+                puntos1 += 1;
+            }else if (nRandom1 == 3 && nRandom2 == 3 ) {
+                System.out.println("Jugador      1           2");
+                System.out.println("             Tijera      Tijera");
+                System.out.println("Empate\n");
             }
-
-            if (opcion < 1 || opcion > 5) {
-                System.out.println("Esta opcion no esta, intente de nuevo.");
-                i--;
-                continue;
-            }
-
-            double precioProducto = precios.get(opcion - 1);
-
-            if (totalCompra + precioProducto > dineroDis) {
-                System.out.println("Falta plata. No puede comprar este producto.");
-                continue;
-            }
-
-            totalCompra += precioProducto;
-            System.out.println("Agregado: " + productos.get(opcion - 1) + " --> $" + precioProducto);
-        }
-
-        double cambio = dineroDis - totalCompra;
-        System.out.println("Total a pagar: $" + totalCompra);
-        System.out.println("Cambio: $" + cambio);
-        System.out.println("Gracias por su compra señor");
+            rondas+=1;
+        }while(puntos1 < 2 || puntos2 < 2);
+        System.out.printf("La cantidad de rondas fue: "+ rondas);
     }
 }
