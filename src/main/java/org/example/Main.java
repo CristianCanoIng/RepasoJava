4b98fe03cf68dca8fe5a1e7ca28ff610c6f3fc60
CristianCanoIng <canox09@gmail.com>
2025-03-02 16:23:41 -0500

    Batalla pokemon

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,72 +1,66 @@
 package org.example;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        Random aleatorio = new Random();
-        int puntos1 = 0;
-        int puntos2 = 0;
-        int rondas = 0;
-        do {
-            int nRandom1 = aleatorio.nextInt(1, 3);
-            int nRandom2 = aleatorio.nextInt(1, 3);
-            if (nRandom1 == 1 && nRandom2 == 1 ) {
-                System.out.println("Jugador      1           2");
-                System.out.println("             Piedra      Piedra");
-                System.out.println("Empate\n");
-            } else if (nRandom1 == 1 && nRandom2 == 2) {
-                System.out.println("Jugador      1           2");
-                System.out.println("             Piedra      Papel");
-                System.out.println("Punto para el jugador 2\n");
-                puntos2 = puntos2+1;
-            } else if (nRandom1 == 1 && nRandom2 == 3) {
-                System.out.println("Jugador      1           2");
-                System.out.println("             Piedra      Tijera");
-                System.out.println("Punto para el jugador 1\n");
-                puntos2 = puntos1+1;
-            } else if (nRandom1 == 2 && nRandom2 == 1) {
-                System.out.println("Jugador      1           2");
-                System.out.println("             Papel       Piedra");
-                System.out.println("Punto para el jugador 1\n");
-                puntos1 = puntos1+1;
-            }else if (nRandom1 == 2 && nRandom2 == 2 ) {
-                System.out.println("Jugador      1           2");
-                System.out.println("             Papel       Papel");
-                System.out.println("Empate\n");
-            } else if (nRandom1 == 2 && nRandom2 == 3) {
-                System.out.println("Jugador      1           2");
-                System.out.println("             Papel       Tijera");
-                System.out.println("Punto para el jugador 2\n");
-                puntos2 = puntos2+1;
-            } else if (nRandom1 == 3 && nRandom2 == 1) {
-                System.out.println("Jugador      1           2");
-                System.out.println("             Tijera      Piedra");
-                System.out.println("Punto para el jugador 2\n");
-                puntos2 = puntos2+1;
-            } else if (nRandom1 == 3 && nRandom2 == 2) {
-                System.out.println("Jugador      1           2");
-                System.out.println("             Tijera       Papel");
-                System.out.println("Punto para el jugador 1\n");
-                puntos1 = puntos1+1;
-            }else if (nRandom1 == 3 && nRandom2 == 3 ) {
-                System.out.println("Jugador      1           2");
-                System.out.println("             Tijera      Tijera");
-                System.out.println("Empate\n");
-            }else{
-                System.out.println("Error");
-            }
-            rondas+=1;
-            System.out.println("puntos ");
-            System.out.println(puntos1 + "  " + puntos2);
-        }while(puntos1 != 2 && puntos2 != 2);
+        //Batalla pokemoncia
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
 
-        if(puntos1==2){
-            System.out.println("Ganador jugador 1");
-        }else{
-            System.out.println("Ganador jugador 2");
+        int vida1 = 200, vida2 = 200;
+        boolean turnoJugador1 = true;
+
+        while (vida1 > 0 && vida2 > 0) {
+            System.out.println("\nTurno del " + (turnoJugador1 ? "Jugador 1" : "Jugador 2"));
+
+            System.out.println("Seleccione el tipo del Pokémon atacante: 1. Agua, 2. Fuego, 3. Planta, 4. Eléctrico");
+            int tipoAtacante = sc.nextInt();
+
+            System.out.println("Seleccione el tipo del Pokémon defensor: 1. Agua, 2. Fuego, 3. Planta, 4. Eléctrico");
+            int tipoDefensor = sc.nextInt();
+
+            int ataque = rand.nextInt(100) + 1;
+            int defensa = rand.nextInt(100) + 1;
+
+            System.out.println("Ataque generado: " + ataque);
+            System.out.println("Defensa generada: " + defensa);
+
+            double efectividad = 1.0;
+
+            if ((tipoAtacante == 1 && tipoDefensor == 2) ||
+                    (tipoAtacante == 2 && tipoDefensor == 3) ||
+                    (tipoAtacante == 3 && tipoDefensor == 1) ||
+                    (tipoAtacante == 4 && tipoDefensor == 1)) {
+                efectividad = 2.0;
+            } else if ((tipoAtacante == 2 && tipoDefensor == 1) ||
+                    (tipoAtacante == 3 && tipoDefensor == 2) ||
+                    (tipoAtacante == 1 && tipoDefensor == 3) ||
+                    (tipoAtacante == 1 && tipoDefensor == 4)) {
+                efectividad = 0.5;
+            }
+
+            double dano = 50 * ((double) ataque / defensa) * efectividad;
+
+            if (turnoJugador1) {
+                vida2 -= dano;
+                System.out.println("Jugador 2 recibe " + dano + " de daño. Vida restante: " + Math.max(0, vida2));
+            } else {
+                vida1 -= dano;
+                System.out.println("Jugador 1 recibe " + dano + " de daño. Vida restante: " + Math.max(0, vida1));
+            }
+
+            turnoJugador1 = !turnoJugador1;
         }
-        System.out.printf("La cantidad de rondas fue: "+ rondas);
+
+        System.out.println("\n¡Juego terminado!");
+        if (vida1 <= 0) {
+            System.out.println("Jugador 2 gana la batalla!");
+        } else {
+            System.out.println("Jugador 1 gana la batalla!");
+        }
+
     }
 }
